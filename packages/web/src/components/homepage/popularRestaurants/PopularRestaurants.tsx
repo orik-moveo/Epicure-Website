50dce90ca3fa35d1244efd2a3052352e170a02ee
orik-moveo <ori.kenigsberg@moveo.co.il>
2025-12-22 17:03:25 +0200

    refactor: create generic Card component and reusable CardsCarousel
    - Extract generic Card component for reusable card layout (image, title, subtitle, bottomContent)
    - Refactor RestaurantCard to use generic Card component
    - Create CardsCarousel component for horizontal scroll on mobile
    - Update PopularRestaurants to use CardsCarousel

--- packages/web/src/components/homepage/popularRestaurants/PopularRestaurants.tsx
+++ packages/web/src/components/homepage/popularRestaurants/PopularRestaurants.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useIsMobile } from '../../../hooks/useIsMobile';
+import CardsCarousel from '../../CardsCarousel/CardsCarousel';
 import RestaurantCard from '../../restaurants/RestaurantCard';
 import styles from './PopularRestaurants.module.scss';
 
@@ -40,16 +41,17 @@ export default function PopularRestaurants({
     <section className={isMobile ? styles.mobile : styles.desktop}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardsContainer}>
-        {restaurants.map((restaurant, index) => (
-          <RestaurantCard
-            key={index}
-            image={restaurant.image[0]}
-            name={restaurant.name}
-            chefName={restaurant.chef.name}
-            rating={restaurant.rating}
-            isMobile={isMobile}
-          />
-        ))}
+        <CardsCarousel>
+          {restaurants.map((restaurant, index) => (
+            <RestaurantCard
+              key={index}
+              image={restaurant.image[0]}
+              name={restaurant.name}
+              chefName={restaurant.chef.name}
+              rating={restaurant.rating}
+            />
+          ))}
+        </CardsCarousel>
       </div>
       {!isMobile && (
         <div className={styles.allRestaurantsLink}>
