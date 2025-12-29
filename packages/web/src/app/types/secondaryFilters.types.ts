850e5fb9743e0db0272e30596b59bdd6dcf95aaf
orik-moveo <ori.kenigsberg@moveo.co.il>
2025-12-29 13:16:14 +0200

    refactor: improve filters with Next.js Image and refactor type exports
    - Use Next.js Image component in SecondaryFilters instead of img tag
    - Export primaryFilterOptions from filters.types.ts
    - Export secondaryFilterOptions from secondaryFilters.types.ts

---
+++ packages/web/src/app/types/secondaryFilters.types.ts
@@ -0,0 +1,7 @@
+export type SecondaryFilterOption = 'priceRange' | 'distance' | 'rating';
+
+export const secondaryFilterOptions: SecondaryFilterOption[] = [
+  'priceRange',
+  'distance',
+  'rating',
+];
