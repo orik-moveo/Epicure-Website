e75509b6b2e92a90bf12e8eda7324f37f9953f46
orik-moveo <ori.kenigsberg@moveo.co.il>
2025-12-29 15:20:12 +0200

    feat(restaurants): update Restaurant type to support filtering logic
    - Added isPopular boolean
    - Added createdAt string
    - Added openingHours interface
    - Added location interface
    Extended Restaurant type with new fields for upcoming filtering logic

--- packages/web/src/app/types/restaurants.types.ts
+++ packages/web/src/app/types/restaurants.types.ts
@@ -1,3 +1,23 @@
+export interface OpeningHours {
+  day:
+    | 'sunday'
+    | 'monday'
+    | 'tuesday'
+    | 'wednesday'
+    | 'thursday'
+    | 'friday'
+    | 'saturday';
+  open: string; // פורמט: "HH:mm"
+  close: string; // פורמט: "HH:mm"
+}
+
+export interface Location {
+  title: string;
+  city: string;
+  lat: number;
+  lng: number;
+}
+
 export interface Restaurant {
   image: Array<{
     url: string;
@@ -9,4 +29,8 @@ export interface Restaurant {
     name: string;
   };
   rating: number;
+  openingHours?: OpeningHours[];
+  location?: Location;
+  isPopular?: boolean;
+  createdAt?: string;
 }
