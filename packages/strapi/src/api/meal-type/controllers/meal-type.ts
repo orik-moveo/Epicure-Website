875af28d87d67bb32b4aab7197997e99bc59d84b
orik-moveo <ori.kenigsberg@moveo.co.il>
2026-01-05 13:04:10 +0200

    feat: add MealType collection
    - Created MealType collection (breakfast, lunch, dinner
    - Linked Dishes to MealTypes via Many-to-Many relation
    - Purpose: enable menu filtering by meal type on the restaurant page

---
+++ packages/strapi/src/api/meal-type/controllers/meal-type.ts
@@ -0,0 +1,7 @@
+/**
+ * meal-type controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::meal-type.meal-type');
---
+++ packages/strapi/src/api/meal-type/services/meal-type.ts
@@ -0,0 +1,7 @@
+/**
+ * meal-type service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::meal-type.meal-type');
